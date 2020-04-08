aa7fd4d13e01cbe9b58aa8050afbbbae1230e7c3
Sebastian Rothe <rothe@infai.org>
2020-04-09 00:30:07 +0200

    rename theme build output

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 VERSION=${1:-develop}
 
-zip -r -9 dist/gruseltour-leipzig-twentytwenty-$VERSION.zip src \
+zip -r -9 dist/gruseltourleipzig-twentytwenty-$VERSION.zip src \
     -x */.DS_Store \
     -x */.git \
     -x */.svn \
