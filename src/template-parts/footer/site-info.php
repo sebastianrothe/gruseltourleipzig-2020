f9163b4a7d91c8c95673861567fe2981b0c9c6bb
Sebastian Rothe <rothe@infai.org>
2020-04-08 23:49:26 +0200

    remove unused tracking code

--- src/template-parts/footer/site-info.php
+++ src/template-parts/footer/site-info.php
@@ -21,5 +21,3 @@ if (function_exists('the_privacy_policy_link')) {
 	<span role="separator" aria-hidden="true"></span>
 	<span>Unsere besonderen Stadtführungen gibt es auch in anderen Städten: <a href="https://gruseltour-berlin.de/">Gruseltour Berlin</a></span>
 </div><!-- .site-info -->
-
-<?php get_template_part( 'template-parts/footer/form', 'tracking' ); ?>
